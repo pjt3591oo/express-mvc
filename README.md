496acbb2cf69f2ba5f633eabda725fb42f7c83c2
pjt3591oo <pjt3591oo@gmail.com>
2021-01-20 21:01:15 +0900

    README.md 파일에 .env를 수정한다

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 [블로그 설명 바로가기](https://blog.naver.com/pjt3591oo/222212372082)
 
+* 환경변수
+
+`.env`
+
+```sh
+port=3000
+mongoURI=mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false
+```
+
 * 의존성 모듈 설치
 
 ```sh
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // .env를 process.env로 가져오기 위한
 require('dotenv').config();
 
+const createError = require('http-errors');
+
 // 모듈 import
 const express = require('express');
 const mongoose =  require("mongoose");
@@ -23,6 +25,24 @@ app.use(bodyParser.json());
 
 // router
 app.use('/article', articleRouter);
+
+// 404 처리
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// 다음 미들웨어는 next(err)이 호출되면 해당 미들웨어의 콜백이 동작
+// 에러 미들웨어는 err을 포함하여 4개의 인자를 전달받는 특수한 형태의 미들웨어
+// 에러 핸들러
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({data: "", code: ""});
+});
 
 // server 실행
 app.listen(port, () => {
